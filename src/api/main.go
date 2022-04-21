cf9e6b43aa44b2a29262ec903b580eaa0f6b1b2f
POTCHONA Essosolam Justin <potchjust@gmail.com>
2022-04-21 23:48:18 +0000

    [api] define websocket

--- src/api/main.go
+++ src/api/main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+
+	// for development pupose no check will be done actually
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Host)
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	reader(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println(writer, "Simple server ")
+	})
+	// listen the websocket connection
+	http.HandleFunc("/ws", serveWs)
+}
 
 func main() {
-	fmt.Println("Chat app ")
+	fmt.Println("Chat App v0.01")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
 }
